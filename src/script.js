290353a3e965b39c3eccebd610875a96a748efaa
Kevin Tran <kthightran024@gmail.com>
2024-04-09 21:58:13 -0700

    added twitter and facebook characters left counter

--- src/script.js
+++ src/script.js
@@ -1,17 +1,24 @@
 const textareaEL = document.querySelector('.textarea');
+const wordsStatEl = document.querySelector('.stat__number--words');
 const charactersStatEl = document.querySelector('.stat__number--characters');
+const twitterStatEl = document.querySelector('.stat__number--twitter');
+const facebookStatEl = document.querySelector('.stat__number--facebook');
+
 
 textareaEL.addEventListener('input', () => {
   // words
   
   // characters
   const numberOfCharacters = textareaEL.value.length;
-  charactersStatEl.innerText = (numberOfCharacters);
+  charactersStatEl.textContent = numberOfCharacters;
 
   // twitter
-  
-  charactersStatEl.innerText = (numberOfCharacters);
+  const twitterCharLeft = 280 - numberOfCharacters;
+  twitterStatEl.textContent = twitterCharLeft;
 
   // facebook
+  const facebookCharLeft = 2200 - numberOfCharacters;
+  facebookStatEl.textContent = facebookCharLeft;
 
+  // 
 });
